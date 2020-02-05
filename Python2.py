e0ea81c77ba0fde5e695a93fe5f8ea9ec7b1be81
SumaiyaSirajudeen <60500200+SumaiyaSirajudeen@users.noreply.github.com>
2020-02-05 11:34:54 +0530

    Add files via upload

---
+++ Python2.py
@@ -0,0 +1,3 @@
+a = 'HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain'
+b = '22', '27', '97', '102'
+print (a[0:6] + ' ' + a[6:12])
