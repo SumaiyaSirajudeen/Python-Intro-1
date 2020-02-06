4be83613ea7b66b0a000e8784f52ac2153a36d4a
SumaiyaSirajudeen <60500200+SumaiyaSirajudeen@users.noreply.github.com>
2020-02-06 10:43:41 +0530

    Add files via upload

---
+++ Problem2.py
@@ -0,0 +1,4 @@
+a = 'HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain'
+print (a[0:6] + ' ' + a[6:12])
+print (a[22:27] + a[27:28] + ' ' + a[97:102] + a[102:103])
+print (a[22:27] + a[27] + ' ' + a[97:102] + a[102])
