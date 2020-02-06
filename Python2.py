810b325e268ad9464d33772fe620cb4d97916de1
SumaiyaSirajudeen <60500200+SumaiyaSirajudeen@users.noreply.github.com>
2020-02-06 10:33:20 +0530

    Delete Python2.py

--- Python2.py
+++
@@ -1,3 +0,0 @@
-a = 'HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain'
-b = '22', '27', '97', '102'
-print (a[0:6] + ' ' + a[6:12])
